f83bd1cec2390bf9c909e23f562dd572f6a94baf
Sami Zirak <samizirakdev@gmail.com>
2018-05-15 13:19:46 +0000

    Crates & Crate Drops
    Created Crate Drop script which on destruction of a crate will drop a random item. Also created basic prefabs of Fire Axe & Crowbar.

---
+++ Assets/Scripts/CrateDrops.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateDrops : MonoBehaviour {
+
+    public GameObject[] possibleDrops;
+
+    public Sprite [] crateSprites;
+
+    public int health = 3;
+
+    private int dropRNGValue;
+
+    public int generatedRNGValue;
+
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start ()
+    {
+        dropRNGValue = possibleDrops.Length;
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(health == 2)
+        {
+            sr.sprite = crateSprites[0];
+        }
+
+        if(health == 1)
+        {
+            sr.sprite = crateSprites[1];
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        generatedRNGValue = Random.Range(0, dropRNGValue);
+
+        Debug.Log("" + generatedRNGValue);
+
+        Instantiate(possibleDrops[generatedRNGValue], transform.position,Quaternion.Euler(0,0,0));
+
+    }
+
+}
